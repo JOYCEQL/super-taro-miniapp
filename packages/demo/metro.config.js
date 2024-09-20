7eae35aaa3d1635c957fb17308d6177f4c1ec0f1
yuguangzhou <yuguangzhou@yiwise.com>
2024-09-21 00:54:21 +0800

    feat: rn test

---
+++ packages/demo/metro.config.js
@@ -0,0 +1,27 @@
+const { getDefaultConfig, mergeConfig } = require('@react-native/metro-config');
+const { getMetroConfig } = require('@tarojs/rn-supporter');
+const path = require('path');
+
+const filePath = path.resolve(__dirname, '../taro-native-shell');
+
+/**
+ * Metro configuration
+ * https://facebook.github.io/metro/docs/configuration
+ *
+ * @type {import('metro-config').MetroConfig}
+ */
+const config = {
+  watchFolders: [path.resolve(__dirname, '../../')],
+  resolver: {
+    unstable_enableSymlinks: true,
+    unstable_enablePackageExports: true,
+  },
+};
+console.log(__dirname, 'getDefaultConfig(__dirname)');
+module.exports = (async function () {
+  return mergeConfig(
+    getDefaultConfig(__dirname),
+    await getMetroConfig(),
+    config
+  );
+})();
